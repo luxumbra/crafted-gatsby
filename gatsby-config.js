8d311ae705ec8324db1453300d3950cf92d202d4
luxumbra <hello@crafted.im>
2019-01-11 00:35:14 +0000

    trying to get cms to write files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,8 +68,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
+        name: "Crafted",
+        short_name: "Crafted",
         start_url: "/",
         background_color: "#6b37bf",
         theme_color: "#6b37bf",
